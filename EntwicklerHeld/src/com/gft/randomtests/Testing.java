1b5231d061067e16aa49374a33009b189cf48c20
cmen <cmen@GFT-t8diRfYD45i.gft.com>
2023-10-19 17:01:31 +0200

    chore: testing out different things

--- EntwicklerHeld/src/com/gft/randomtests/Testing.java
+++ EntwicklerHeld/src/com/gft/randomtests/Testing.java
@@ -1,9 +1,27 @@
 package com.gft.randomtests;
 
+import java.math.BigInteger;
+
+import javax.naming.spi.DirStateFactory.Result;
+
 public class Testing {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println(Grains.grainsOnSquare(7));
+		
+	}
+
+	public class Grains {
+		
+		public static BigInteger grainsOnSquare(final int square) {
+			if (square < 1 || square > 64) throw new IllegalArgumentException();
+			
+			BigInteger result = BigInteger.valueOf(2).pow(square - 1);
+			
+			return result;
+			
+		}
 
 	}
 
